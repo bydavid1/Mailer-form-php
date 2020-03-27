72eca925543016c9fcfd5d341e0349ebc911749d
byronjd <byronjimenez9911@gmail.com>
2020-03-27 14:08:21 -0600

    serve testing 2

--- includes/sendmail.php
+++ includes/sendmail.php
@@ -1,4 +1,19 @@
 <?php
 
-echo "Si entre";
+require 'includes/phpmailer/PHPMailer.php';
+require 'includes/phpmailer/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+//Response message
+$valid['success'] = array(
+    'success' => false,
+    'messages' => array()
+);
+
+
+    $valid['success']  = true;
+    $valid['messages'] = "Enviado";
+
+echo json_encode($valid);
 ?>
