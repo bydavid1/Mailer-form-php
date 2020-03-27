f2fc2a353753d0a83f99d30bc9c7092dc70349e8
byronjd <byronjimenez9911@gmail.com>
2020-03-27 14:14:23 -0600

    serve test 5

--- includes/sendmail.php
+++ includes/sendmail.php
@@ -1,5 +1,10 @@
 <?php
 
+require 'includes/phpmailer/PHPMailer.php';
+require 'includes/phpmailer/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
 //Response message
 $valid['success'] = array(
     'success' => false,
